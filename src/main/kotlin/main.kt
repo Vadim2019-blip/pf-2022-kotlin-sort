dffc08b9381bd7ae8c5b5f4074722c4c3abb4cb7
Вадим <oxa-182008@yandex.ru>
2023-09-19 00:48:21 +0300

    Start working

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,23 @@
+
+
 fun main(args: Array<String>) {
-    TODO()
+    val lenparse = args.size
+    val ListFlag = arrayOf("-f", "-r", "-u", "-V", "-o")
+    val ListOfType = arrayOf("")
+    val ListOfFiles = arrayOf("")
+
+    for(arg in args){
+        if (arg in ListFlag){
+            ListOfType+arg
+        }
+        else{
+            ListOfFiles+arg
+        }
+    }
+    for(i in ListOfType){
+        println(i)
+    }
+    for(j in ListOfType){
+        println(j)
+    }
 }
